6ea6de27a82144ca5188a5bc266e3e79403c99ed
lbmacuzi <asghedom@adobe.com>
2018-08-30 16:05:53 -0700

    added handlebar conditionals

--- js/main.js
+++ js/main.js
@@ -4,32 +4,17 @@ $( document ).ready(function() {
     type:"GET",
     url:"js/test.json",
     success: function(data) {
-      console.log(data);
-      var rowScript = $("#rowTemplate").html();
+      //console.log(data);
       var mainInfoScript = $("#mainInfo").html();
-    
-      Handlebars.registerHelper('list', function(items, options) {
-        var out = ""; 
-        for(var i=0, l=items.length; i<l; i++) {
-          out = out + options.fn(items[i]);
-          console.log(options.fn(items[i]));
-        }
-        return out;
-      });
 
       // Compiling the templates
-      var rowTemplate = Handlebars.compile(rowScript);
       var mainInfoTemplate = Handlebars.compile(mainInfoScript);
 
       // Pass our data to the template
-      var rowCompiledHtml = rowTemplate(data);
       var mainInfoCompiledHtml = mainInfoTemplate(data);
-      // Write your code here
-
-
+      
       // Add the compiled html to the page
-      $(".tableBody").html(rowCompiledHtml);
-      $(".basicInfo").html(mainInfoCompiledHtml);
+      $(".container").html(mainInfoCompiledHtml);
     },
     dataType: 'json',
   });
